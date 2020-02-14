dbfb4d402373e75dd20921bf9328f6d1f317f22a
Robin <robinprodan7373@gmail.com>
2020-02-14 10:00:28 +0600

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
 
-Vue.config.productionTip = false
+import  BootstrapVue from 'bootstrap-vue';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+import App from './App.vue';
+
+Vue.use(BootstrapVue);
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
